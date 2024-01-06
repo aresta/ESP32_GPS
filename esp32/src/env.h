220f3e0c6aa86c574648a1a475096c045aebdce6
gos <6231546+aresta@users.noreply.github.com>
2024-01-06 10:38:01 +0100

    wip

---
+++ esp32/src/env.h
@@ -0,0 +1,9 @@
+#ifndef env_h_
+#define env_h_
+
+// #define INIT_POS -530000, 5340000 // picos
+// #define INIT_POS 235664.91, 5074788.07 // Tibidabo
+// #define INIT_POS 244808.69, 5070020.31 // bcn
+#define INIT_POS 224672.31, 5107378.91 // La Mola
+
+#endif
