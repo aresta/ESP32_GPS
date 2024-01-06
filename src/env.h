d4e60e2b554d4f21acd6a1f2f54d554a04e0d029
gos <6231546+aresta@users.noreply.github.com>
2024-01-06 11:13:04 +0100

    fix

--- src/env.h
+++ src/env.h
@@ -5,6 +5,5 @@
 // #define INIT_POS 235664.91, 5074788.07 // Tibidabo
 // #define INIT_POS 244808.69, 5070020.31 // bcn
 // #define INIT_POS 224672.31, 5107378.91 // La Mola
-#define INIT_POS -530000, 5340000
 
 #endif
